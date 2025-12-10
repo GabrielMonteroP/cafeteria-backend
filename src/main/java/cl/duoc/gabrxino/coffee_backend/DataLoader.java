4e586224c5ffa567b842c2c79c093e3b24653bb9
GabrielPalmaMontero <gabr.palma@duocuc.cl>
2025-12-10 14:04:13 -0300

    Correccion fotos que no cargaban

--- src/main/java/cl/duoc/gabrxino/coffee_backend/DataLoader.java
+++ src/main/java/cl/duoc/gabrxino/coffee_backend/DataLoader.java
@@ -37,20 +37,20 @@ public class DataLoader implements CommandLineRunner {
         crear("Cappuccino Tradicional", "Espresso, leche y mucha espuma", 2800, "CAFE", "https://images.unsplash.com/photo-1572442388796-11668a67e53d?w=500");
         crear("Cappuccino Vainilla", "Con jarabe de vainilla natural", 3200, "CAFE", "https://images.unsplash.com/photo-1572442388796-11668a67e53d?w=500");
         crear("Cappuccino Avellana", "Dulce toque de avellana tostada", 3200, "CAFE", "https://images.unsplash.com/photo-1572442388796-11668a67e53d?w=500");
-        crear("Latte Macchiato", "Leche manchada con espresso", 2900, "CAFE", "https://images.unsplash.com/photo-1593443320739-77f74952dabd?w=500");
-        crear("Mocaccino", "Café, leche y salsa de chocolate", 3400, "CAFE", "https://images.unsplash.com/photo-1550681566-f1c305a2b376?w=500");
+        crear("Latte Macchiato", "Leche manchada con espresso", 2900, "CAFE", "https://assets.tmecosys.com/image/upload/t_web_rdp_recipe_584x480_1_5x/img/recipe/ras/Assets/314D11A6-4457-4C70-A1BE-A6C25F597C18/Derivates/B362DC69-6AAA-43E1-AF51-184463E8551B.jpg");
+        crear("Mocaccino", "Café, leche y salsa de chocolate", 3400, "CAFE", "https://www.cafescandelas.com/uploads/media_items/mocaccino-500.500.500.s.jpg");
         crear("Flat White", "Doble espresso con capa fina de leche", 3000, "CAFE", "https://images.unsplash.com/photo-1577968897966-3d4325b36b61?w=500");
-        crear("Caramel Macchiato", "Vainilla, leche, espresso y caramelo", 3600, "CAFE", "https://images.unsplash.com/photo-1461023058943-48dbf1399f98?w=500");
-        crear("Café Irlandés", "Con un toque de whisky y crema", 4500, "CAFE", "https://images.unsplash.com/photo-1518057118035-76b4d32a0c4f?w=500");
-        crear("Affogato", "Espresso sobre helado de vainilla", 3800, "CAFE", "https://images.unsplash.com/photo-1593016250795-327c59c5d0b4?w=500");
-        crear("Chai Latte", "Té especiado con leche cremosa", 3100, "CAFE", "https://images.unsplash.com/photo-1576092762791-aa9c222faeeb?w=500");
-        crear("Dirty Chai", "Chai Latte con un shot de espresso", 3500, "CAFE", "https://images.unsplash.com/photo-1576092762791-aa9c222faeeb?w=500");
+        crear("Caramel Macchiato", "Vainilla, leche, espresso y caramelo", 3600, "CAFE", "https://dinnerthendessert.com/wp-content/uploads/2023/10/Caramel-Macchiato-10.jpg");
+        crear("Café Irlandés", "Con un toque de whisky y crema", 4500, "CAFE", "https://cdn.recetasderechupete.com/wp-content/uploads/2021/03/Irish-coffee-2.jpg");
+        crear("Affogato", "Espresso sobre helado de vainilla", 3800, "CAFE", "https://static01.nyt.com/images/2021/08/15/magazine/affogato/affogato-mediumSquareAt3X-v2.jpg");
+        crear("Chai Latte", "Té especiado con leche cremosa", 3100, "CAFE", "https://carorocco.com/wp-content/uploads/2021/03/Te-Chai-Latte-CUERPO.jpg");
+        crear("Dirty Chai", "Chai Latte con un shot de espresso", 3500, "CAFE", "https://midwestniceblog.com/wp-content/uploads/2022/09/homemade-dirty-chai-latte-recipe.jpg");
     }
 
     private void cargarPasteleria() {
-        crear("Medialuna", "Clásica argentina, dulce y mantecosa", 1200, "PASTELERIA", "https://images.unsplash.com/photo-1555507036-ab1f4038808a?w=500");
+        crear("Medialuna", "Clásica, dulce y mantecosa", 1200, "PASTELERIA", "https://images.unsplash.com/photo-1555507036-ab1f4038808a?w=500");
         crear("Croissant Francés", "Hojaldre crujiente con mantequilla", 1800, "PASTELERIA", "https://images.unsplash.com/photo-1555507036-ab1f4038808a?w=500");
-        crear("Croissant Almendras", "Relleno de crema de almendras", 2400, "PASTELERIA", "https://images.unsplash.com/photo-1509440159596-0249088772ff?w=500");
+        crear("Croissant Almendras", "Relleno de crema de almendras", 2400, "PASTELERIA", "https://images.unsplash.com/photo-1555507036-ab1f4038808a?w=500");
         crear("Muffin Arándano", "Esponjoso con arándanos frescos", 2000, "PASTELERIA", "https://images.unsplash.com/photo-1607958996333-41aef7caefaa?w=500");
         crear("Muffin Chocolate", "Doble chocolate con chips", 2100, "PASTELERIA", "https://images.unsplash.com/photo-1607958996333-41aef7caefaa?w=500");
         crear("Donut Glaseada", "Clásica dona con glaseado de azúcar", 1500, "PASTELERIA", "https://images.unsplash.com/photo-1551024506-0bccd828d307?w=500");
@@ -59,8 +59,8 @@ public class DataLoader implements CommandLineRunner {
         crear("Cheesecake Frutos Rojos", "Porción de tarta de queso estilo NY", 3500, "PASTELERIA", "https://images.unsplash.com/photo-1524351199678-941a58a3df50?w=500");
         crear("Pie de Limón", "Base crujiente con merengue italiano", 3200, "PASTELERIA", "https://images.unsplash.com/photo-1519915028121-7d3463d20b13?w=500");
         crear("Torta de Zanahoria", "Con frosting de queso crema y nueces", 3400, "PASTELERIA", "https://images.unsplash.com/photo-1621303837174-89787a7d4729?w=500");
-        crear("Galletón de Avena", "Saludable, con pasas y miel", 1500, "PASTELERIA", "https://images.unsplash.com/photo-1499636138143-bd649043ea52?w=500");
-        crear("Galletón ChocoChips", "Estilo americano, borde crujiente", 1600, "PASTELERIA", "https://images.unsplash.com/photo-1499636138143-bd649043ea52?w=500");
+        crear("Galletón de Avena", "Saludable, con pasas y miel", 1500, "PASTELERIA", "https://images.unsplash.com/photo-1555507036-ab1f4038808a?w=500");
+        crear("Galletón ChocoChips", "Estilo americano, borde crujiente", 1600, "PASTELERIA", "https://images.unsplash.com/photo-1555507036-ab1f4038808a?w=500");
         crear("Cinnamon Roll", "Rollo de canela con glaseado", 2300, "PASTELERIA", "https://images.unsplash.com/photo-1509365465985-25d11c17e812?w=500");
         crear("Tiramisú", "Postre italiano con café y mascarpone", 3800, "PASTELERIA", "https://images.unsplash.com/photo-1571877227200-a0d98ea607e9?w=500");
     }
@@ -79,15 +79,15 @@ public class DataLoader implements CommandLineRunner {
     }
 
     private void cargarBebidasFrias() {
-        crear("Iced Coffee", "Café negro con hielo", 2500, "FRIO", "https://images.unsplash.com/photo-1517701604599-bb29b5c7fa5b?w=500");
-        crear("Iced Latte", "Latte frío servido en capas", 3000, "FRIO", "https://images.unsplash.com/photo-1517701604599-bb29b5c7fa5b?w=500");
-        crear("Cold Brew", "Infusión en frío por 12 horas", 3200, "FRIO", "https://images.unsplash.com/photo-1517701604599-bb29b5c7fa5b?w=500");
-        crear("Frappuccino Café", "Granizado de café con crema", 3800, "FRIO", "https://images.unsplash.com/photo-1517701604599-bb29b5c7fa5b?w=500");
-        crear("Frappuccino Moka", "Granizado con chocolate", 4000, "FRIO", "https://images.unsplash.com/photo-1517701604599-bb29b5c7fa5b?w=500");
-        crear("Frappuccino Caramelo", "El favorito, muy dulce", 4000, "FRIO", "https://images.unsplash.com/photo-1517701604599-bb29b5c7fa5b?w=500");
+        crear("Iced Coffee", "Café negro con hielo", 2500, "FRIO", "https://cdn.loveandlemons.com/wp-content/uploads/2025/05/how-to-make-iced-coffee-at-home.jpg");
+        crear("Iced Latte", "Latte frío servido en capas", 3000, "FRIO", "https://cdn.loveandlemons.com/wp-content/uploads/2025/05/how-to-make-iced-coffee-at-home.jpg");
+        crear("Cold Brew", "Infusión en frío por 12 horas", 3200, "FRIO", "https://lifesimplified.gorenje.com/wp-content/uploads/2024/06/gorenje-blog-refreshing_cold_brew_coffee-1040x585.jpg");
+        crear("Frappuccino Café", "Granizado de café con crema", 3800, "FRIO", "https://simplyhomecooked.com/wp-content/uploads/2025/07/caramel-frappuccino-recipe-2.jpg");
+        crear("Frappuccino Moka", "Granizado con chocolate", 4000, "FRIO", "https://simplyhomecooked.com/wp-content/uploads/2025/07/caramel-frappuccino-recipe-2.jpg");
+        crear("Frappuccino Caramelo", "El favorito, muy dulce", 4000, "FRIO", "https://simplyhomecooked.com/wp-content/uploads/2025/07/caramel-frappuccino-recipe-2.jpg");
         crear("Jugo Naranja", "Recién exprimido", 2800, "FRIO", "https://images.unsplash.com/photo-1600271886742-f049cd451bba?w=500");
         crear("Limonada Menta", "Refrescante y natural", 2800, "FRIO", "https://images.unsplash.com/photo-1513558161293-cdaf765ed2fd?w=500");
-        crear("Agua Mineral", "Con o sin gas", 1500, "FRIO", "https://images.unsplash.com/photo-1560706834-bed1cad2a2af?w=500");
+        crear("Agua Mineral", "Con o sin gas", 1500, "FRIO", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAmASyPCeCGrZo7uKnbGbpnT4p6J5c89BrYA&s");
         crear("Bebida Lata", "Coca-Cola, Sprite, Fanta", 1500, "FRIO", "https://images.unsplash.com/photo-1622483767028-3f66f32aef97?w=500");
     }
 
